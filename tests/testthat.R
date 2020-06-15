697651acd60b8a576ebcbf6c3dc833e4ecba679e
Aseem Gill <aseem.gill01@gmail.com>
2020-06-15 17:24:12 -0400

    as

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
-devtools::install_github("https://github.com/AseemGill/farsss/tree/master/inst/extdata")
+# devtools::install_github("https://github.com/AseemGill/farsss")
 expect_that(make_filename(2015), is_a("character"))
